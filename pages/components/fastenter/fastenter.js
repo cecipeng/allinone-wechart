5d1e45af5d52e11cb9f965d0b7f03656f60dc120
jiexi <1047328479@qq.com>
2018-07-13 13:25:02 +0800

    init

---
+++ pages/components/fastenter/fastenter.js
@@ -0,0 +1,31 @@
+// pages/common/fastenter/fastenter.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    fastEnterList : {
+      type: Array,
+      value: [
+        {
+          "url": "../index/index",
+          "classname": "ss",
+          "name": "ss"
+        }
+      ]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
